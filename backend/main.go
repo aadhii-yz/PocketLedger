22e187188aac66b1a6a1d5e47da6aa87e0613a60
Adithya <aadhii.yz@gmail.com>
2026-04-21 10:47:36 +0530

    added license and gitignores

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
-	"log"
 )
 
 func main() {
 	app := pocketbase.New()
 
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		// Register custom POS/Billing routes
-		// e.Router.Add("POST", "/api/custom/checkout", checkoutHandler(app))
-		return nil
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// registers new "GET /hello" route
+		se.Router.GET("/hello", func(re *core.RequestEvent) error {
+			return re.String(200, "Hello world!")
+		})
+
+		return se.Next()
 	})
 
 	if err := app.Start(); err != nil {
