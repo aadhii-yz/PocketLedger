b079d4636098bd8d46b17bd1d4cea2166c2e2926
Adithya <aadhii.yz@gmail.com>
2026-04-22 12:44:28 +0530

    added dashboard handler

--- backend/handlers/stats.go
+++ backend/handlers/stats.go
@@ -1,9 +1,18 @@
 package handlers
 
-import "github.com/pocketbase/pocketbase/core"
+import (
+	"net/http"
+
+	"github.com/aadhii-yz/PocketLedger/backend/services"
+	"github.com/pocketbase/pocketbase/core"
+)
 
 func Dashboard(app core.App) func(*core.RequestEvent) error {
 	return func(e *core.RequestEvent) error {
-		return nil
+		stats, err := services.GetDashboard(app)
+		if err != nil {
+			return e.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+		}
+		return e.JSON(http.StatusOK, stats)
 	}
 }
