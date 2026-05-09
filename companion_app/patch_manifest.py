adb9234c9817b98347bbd79b075b00f4b16fa7dc
Adithya <aadhii.yz@gmail.com>
2026-05-09 16:18:17 +0530

    fix manifest error

--- companion_app/patch_manifest.py
+++ companion_app/patch_manifest.py
@@ -21,11 +21,24 @@ service = (
     '\n        <service\n'
     '            android:name="id.flutter.flutter_background_service.BackgroundService"\n'
     '            android:foregroundServiceType="dataSync"\n'
-    '            android:exported="false"/>'
+    '            android:exported="false"\n'
+    '            tools:replace="android:exported"/>'
 )
 
 content = content.replace('    <application', permissions + '<application', 1)
 content = content.replace('</application>', service + '\n    </application>', 1)
+# Add tools namespace to <manifest> if not already present
+if 'xmlns:tools' not in content:
+    content = content.replace(
+        '<manifest xmlns:android=',
+        '<manifest xmlns:android=',
+        1,
+    )
+    content = content.replace(
+        'xmlns:android="http://schemas.android.com/apk/res/android"',
+        'xmlns:android="http://schemas.android.com/apk/res/android"\n    xmlns:tools="http://schemas.android.com/tools"',
+        1,
+    )
 
 with open(path, 'w') as f:
     f.write(content)
